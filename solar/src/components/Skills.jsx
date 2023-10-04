988dacb5716396efae52074ab4958cacff6b7058
samuel <olaoluwasamuel119@gmail.com>
2023-10-04 15:41:58 -0600

    first commit

---
+++ solar/src/components/Skills.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div
+      name="Our Service"
+      className="w-full lg:h-screen bg-white text-gray-300"
+    >
+      <div className="text-white mt-[70px] flex flex-col  px-8   items-center justify-center lg:flex-row  gap-8">
+        <h1 className="font-semibold text-3xl text-start text-black flex-2">
+          We find the highest and Best use of resource
+        </h1>
+        <p className=" text-black text-start">
+          Solar energy is the beacon of sustainability in our quest for cleaner
+          and more eco-friendly power sources.
+        </p>
+      </div>
+      <div className="flex justify-center gap-10 mt-[40px] w-[80%] mx-auto text-black ">
+        <div className="flex flex-col">
+          <div>
+            <h1 className="font-semibold text-xl text-black text-end">
+              Easy installation
+            </h1>
+            <p className="text-end">
+              Solar energy is the beacon of sustainability in our quest for
+              cleaner and more eco-friendly power sources.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-semibold text-xl text-black">
+              Easy installation
+            </h1>
+            <p className="text-end">
+              Solar energy is the beacon of sustainability in our quest for
+              cleaner and more eco-friendly power sources.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-semibold text-xl text-black">
+              Easy installation
+            </h1>
+            <p className="text-end">
+              Solar energy is the beacon of sustainability in our quest for
+              cleaner and more eco-friendly power sources.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <img
+            src="https://res.cloudinary.com/dutvrzezk/image/upload/v1696448459/solar_eyoezs.webp"
+            alt="solar "
+          />
+        </div>
+        <div className="flex flex-col">
+          <div>
+            <h1 className="font-semibold text-xl text-black text-end">
+              Easy installation
+            </h1>
+            <p className="text-end">
+              Solar energy is the beacon of sustainability in our quest for
+              cleaner and more eco-friendly power sources.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-semibold text-xl text-black text-end">
+              Easy installation
+            </h1>
+            <p className="text-end">
+              Solar energy is the beacon of sustainability in our quest for
+              cleaner and more eco-friendly power sources.
+            </p>
+          </div>
+          <div>
+            <h1 className="font-semibold text-xl text-black text-end">
+              Easy installation
+            </h1>
+            <p className="text-end">
+              Solar energy is the beacon of sustainability in our quest for
+              cleaner and more eco-friendly power sources.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
